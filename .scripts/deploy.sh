33f89d09c83ff73d363ae91aebbbe6d3e49eef66
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-23 07:16:31 -0400

    fix(northbrook): checkout in deployment
    fix(northbrook): fix deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,6 +5,8 @@ if [[ $TRAVIS_BRANCH == "master" ]]; then
   git config --global user.name 'Tylor Steinberger';
   git config --global user.email 'tlsteinberger167@gmail.com';
   git remote set-url origin https://$GH_TOKEN@github.com/northbrookjs/northbrook;
+  git fetch origin
+  git checkout origin master
   git branch --set-upstream-to=origin/master master
 
   bash .scripts/npm-login.sh
