2932b9135482acd978e5b7bfd69c90e57064ea65
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-25 03:10:11 -0400

    fix(northbrook): move deploy script to user travis ci env vars

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
   # setup git stuff
-  git config --global user.name 'Tylor Steinberger';
-  git config --global user.email 'tlsteinberger167@gmail.com';
+  git config --global user.name '$USER_NAME';
+  git config --global user.email '$USER_EMAIL';
   git remote set-url origin https://$GH_TOKEN@github.com/northbrookjs/northbrook;
   git fetch origin;
   git checkout master;
@@ -15,5 +15,5 @@ if [[ $TRAVIS_BRANCH == "master" ]]; then
   npm run build;
 
   # run deployment
-  npm run release;
+  node cli.js release;
 fi
