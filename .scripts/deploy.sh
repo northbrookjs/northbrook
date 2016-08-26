90d32eab6e2ba22bb74e7b4324b30843c155ad48
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-25 23:52:54 -0400

    fix(northbrook): single quotes are not supported by bash

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
   # setup git stuff
-  git config --global user.name '${USER_NAME}';
-  git config --global user.email '${USER_EMAIL}';
+  git config --global user.name "${USER_NAME}"
+  git config --global user.email "${USER_EMAIL}";
   git remote set-url origin https://$GH_TOKEN@github.com/northbrookjs/northbrook;
   git fetch origin;
   git checkout master;
