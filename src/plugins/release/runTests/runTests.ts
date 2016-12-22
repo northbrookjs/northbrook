9bfe73cb97d9076416e2c0922810a20990996d7d
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-22 07:35:10 -0500

    fix(release): remove extra log message

--- src/plugins/release/runTests/runTests.ts
+++ src/plugins/release/runTests/runTests.ts
@@ -4,8 +4,6 @@ import { Stdio, AffectedPackages } from '../../../types';
 import { execute } from '../../../helpers';
 
 export function runTests(directory: string, io: Stdio, _spawn = spawn) {
-  io.stdout.write('Running tests...' + EOL);
-
   return function (affectedPackages: AffectedPackages) {
     return execute('npm', ['test', '--silent'], io, directory, _spawn)
       .then(() => affectedPackages);
